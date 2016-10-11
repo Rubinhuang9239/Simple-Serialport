095b5188b057d9297dae5941d8ae761c1b5df01a
Rubinhuang9239 <huang9239@icloud.com>
2016-10-11 19:20:39 -0400

    upload

---
+++ nodeSerial.js
@@ -0,0 +1,51 @@
+var SerialPort = require("serialport");
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//----------------Route--------------------//
+
+app.use(express.static('public'));
+
+http.listen(3000, function(){
+  	console.log("");
+    console.log("");
+    console.log("---------------|  Roomba Testing  |-----------------");
+    console.log("");
+    console.log("Server is on port 3001");
+});
+
+
+// console.log(serial);
+var port = new SerialPort("/dev/cu.usbmodem1411", {
+  baudRate: 9600,
+});
+
+
+port.on('open', function() {
+	console.log("opened");
+});
+
+var lastSerialData = null;
+
+port.on( 'data', function(data){
+		//console.log(data.toString());
+		lastSerialData = data.toString();
+		if(mySocket != undefined){
+			mySocket.emit("serialData",lastSerialData);
+		}
+});
+
+//-----------------Socket.io----------------//
+
+var mySocket = null;
+
+io.on('connection', function(socket){
+
+	mySocket = socket;
+
+});
+
+
+
