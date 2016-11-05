2ab9e6aa9c3da57480cb288b15dc3e91768206e4
Rubinhuang9239 <huang9239@icloud.com>
2016-11-04 20:57:52 -0400

    update port free and handshake

--- nodeSerial.js
+++ nodeSerial.js
@@ -8,34 +8,68 @@ var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+
 http.listen(3000, function(){
   	console.log("");
     console.log("");
-    console.log("---------------|  Roomba Testing  |-----------------");
+    console.log("---------------| Simple Seiral Example |-----------------");
     console.log("");
     console.log("Server is on port 3000");
 });
 
 
-// console.log(serial);
-var port = new SerialPort("/dev/cu.usbmodem1411", {
-  baudRate: 9600,
-  parser: SerialPort.parsers.readline("\n"),
-});
+var portNameChoice = ["/dev/cu.usbmodem1411", "/dev/cu.usbmodem1421"];
+portName = null;
+
+SerialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+
+    for(i=0; i < portNameChoice.length ;i++){
+
+      if(portNameChoice[i] == port.comName){
+        portName = port.comName;
+
+      }
+
+    }
+
+	});
+
+if(portName != null){
+
+	console.log(portName);
+
+	var port = new SerialPort(portName, {
+	  baudRate: 9600,
+	  parser: SerialPort.parsers.readline("\n"),
+	});
 
 
-port.on('open', function() {
-	console.log("opened");
-});
+	port.on('open', function() {
+		console.log("opened");
 
-var lastSerialData = null;
+		setTimeout(function(){
+			port.write("hi");//hi
+			console.log("say Hi");
+		},2000);	
 
-port.on( 'data', function(data){
-		//console.log(data.toString());
-		lastSerialData = data.toString();
-		if(mySocket != undefined){
-			mySocket.emit("serialData",lastSerialData);
-		}
+	});
+
+	var serialData = null;
+
+	port.on( 'data', function(data){
+			console.log(data.toString());
+			serialData = data.toString();
+
+			if(mySocket != undefined){
+				mySocket.emit("serialData",serialData);
+			}
+
+		port.write(0);			
+	});
+
+}
+
 });
 
 //-----------------Socket.io----------------//
